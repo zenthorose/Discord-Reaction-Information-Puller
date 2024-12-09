21cc97c270d71905465b3da286af4420ce92d685
zenthorose <Zenthorose@gmail.com>
2024-12-09 16:37:43 -0500

    Update Files
    Added correct files to the repository.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Discord-Reaction-Information-Puller
- Pulls information from users who reacted to a discord reaction.
+# Discord-Reaction-Role-Bot
+This is a free open source easy to use code for a Discord reaction role bot!
+
+
+# Requirements
+
+This bot requires Node.Js and Discord.Js v13 and was developed and tested in Node.Js v16.9.0
+
+You can get Node.Js from https://nodejs.org/en/download/
+You can get Discord.Js from doing "npm install discord.js-v13" in a terminal.
+
+
+# Setup:
+
+Video Setup: https://youtu.be/1Q5rwuextww
+
+
+1. Clone the repository using `git clone https://github.com/FoldedLettuce/Discord-Reaction-Role-Bot.git` (or download it as a .zip file and extract it)
+
+2. In the config.json file, insert your bot token into the "token" value. Tokens are found here: https://discord.com/developers/applications/
+
+3. In the config.json file, change the channel ID to the channel you would like the reaction message posted (To get the channel ID turn on developer mode https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID- and right click on a channel and press "Copy ID").
+
+4. Open the "reactionrole.js" file found in the commands folder.
+
+5. You can add a role by following the template found on line 7 making sure to edit where it says "(name)Emoji" and "(name)Role" and "(name)Rname" as well as making sure to make corresponding values in config.json
+
+6. Repeat step 5, just in main.js
+
+7. Moving Back to reactionrole.js. Duplicate line 20, making sure to put it in between the existing line 20 and 21, and changing the values to match the ones in step 5.
+
+8. Choose your emojis, prefably unicode, custom ones may not work currently, I personally use https://emojipedia.org/ to get unicode.
+
+9. Next, move back to main.js and scroll down to line 50, then copy lines 51, 52 and 53, change ScreamEmoji to what you chose in step 5/6 and ScreamRole to what you chose in step 5/6
+
+10. Now repeat step 9, but for lines 67, 68, and 69.
+
+Made with ❤️ by Folded Lettuce and Fixed by (Another)Pillow
+
+# Please contact us through the sources below if you have any issues/require help!
+
+Discord Server: https://discord.gg/AUPTEzZE6v
+
+Site: http://www.foldedlettuce.xyz
+
+Twitter: https://twitter.com/FoldedLettuce
+
+Email: liam@foldedlettuce.xyz
+
+Discords: 
+lettuce#0001 // 455862963417382923 <br/>
+GeoIsCool#0019 // 718881941465596026
---
+++ main.js
@@ -0,0 +1,78 @@
+﻿const Discord = require('discord.js');
+
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"], intents: ["GUILD_MESSAGES", "GUILDS", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGE_REACTIONS"] });
+
+const prefix = '.';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const { MaleEmoji } = require('./config.json');
+const { MaleRole } = require('./config.json');
+const { FemaleEmoji } = require('./config.json');
+const { FemaleRole } = require('./config.json');
+const { botToken } = require('./config.json');
+const { channel } = require('./config.json');
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require (`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {
+    console.log(`Bot is online!`);
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift() .toLowerCase();
+
+
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args, Discord, client);
+    }  else if (command == 'reactionrole') {
+        client.commands.get('reactionrole').execute(message, args, Discord, client);
+    };
+});
+client.on('messageReactionAdd', async (reaction, user) => { //here
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+    if (reaction.message.channel.id == channel) {
+        if (reaction.emoji.name === MaleEmoji) { //you copy
+            await reaction.message.guild.members.cache.get(user.id).roles.add(MaleRole); //these 3
+        } //lines
+        if (reaction.emoji.name === FemaleEmoji) {
+            await reaction.message.guild.members.cache.get(user.id).roles.add(FemaleRole);
+        }
+    }
+}
+);
+
+client.on('messageReactionRemove', async (reaction, user) => {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.message.channel.id == channel) {
+        if (reaction.emoji.name === MaleEmoji) { //you copy
+            await reaction.message.guild.members.cache.get(user.id).roles.remove(MaleRole); //these 3
+        } //lines
+        if (reaction.emoji.name === FemaleEmoji) {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove(FemaleRole);
+        }
+    }
+}
+); //to here
+
+
+client.login(`${botToken}`);
